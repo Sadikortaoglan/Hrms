2963f495a743f74731a5fc323fd96b2345666c75
Sadık Ortaoğlan <sadik.ortaoglan@gmail.com>
2021-06-11 02:41:14 +0300

    general bugfixed (#4)

--- HrmsCF/src/main/java/com/sadik/hrmscf/business/abstracts/UserService.java
+++ HrmsCF/src/main/java/com/sadik/hrmscf/business/abstracts/UserService.java
@@ -1,6 +1,7 @@
 package com.sadik.hrmscf.business.abstracts;
 
 import com.sadik.hrmscf.core.entity.User;
+import com.sadik.hrmscf.core.utilities.result.Result;
 
 import java.util.List;
 
@@ -10,5 +11,9 @@ public interface UserService {
     void delete(int id);
 
     List<User> getAll();
+
+    Result add(User user);
+    boolean findByEmailAndPasswordHash(String email, String password);
+    User findById(int id);
 
 }
