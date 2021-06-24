90cf10685d951b6af9ba285252ff9bd050fadd28
Sadikortaoglan <sadik.ortaoglan@gmail.com>
2021-06-25 00:08:16 +0300

    Added GoggleService

---
+++ HrmsCF/src/main/java/com/sadik/hrmscf/core/adapters/mail/MailManager.java
@@ -0,0 +1,25 @@
+package com.sadik.hrmscf.core.adapters.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailManager implements MailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    @Override
+    public void sendMail(String toEmail, String subject, String body) {
+        SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+        simpleMailMessage.setFrom("sadikortaoglan@gmail.com");
+        simpleMailMessage.setTo(toEmail);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(body);
+        System.out.println("Mesaj Gönderildi");
+
+        javaMailSender.send(simpleMailMessage);
+    }
+}
