c53eab5a67b6ac72bad0ebac574aed9b8cab4c12
Sadikortaoglan <sadik.ortaoglan@gmail.com>
2021-07-30 21:51:34 +0300

    bugs fixed

--- HrmsCF/src/main/java/com/sadik/hrmscf/business/concretes/JobPostingManager.java
+++ HrmsCF/src/main/java/com/sadik/hrmscf/business/concretes/JobPostingManager.java
@@ -6,10 +6,14 @@ import com.sadik.hrmscf.core.utilities.result.DataResult;
 import com.sadik.hrmscf.core.utilities.result.Result;
 import com.sadik.hrmscf.core.utilities.result.SuccessDataResult;
 import com.sadik.hrmscf.core.utilities.result.SuccessResult;
+import com.sadik.hrmscf.dataAccess.abstracts.CityDao;
+import com.sadik.hrmscf.dataAccess.abstracts.EmployerDao;
 import com.sadik.hrmscf.dataAccess.abstracts.JobPostingDao;
 import com.sadik.hrmscf.entities.concretes.JobPosting;
 import com.sadik.hrmscf.entities.dtos.JobPostingForEmployerDto;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,19 +24,41 @@ public class JobPostingManager implements JobPostingService {
 
     private JobPostingDao jobPostingDao;
     private ModelMapper modelMapper;
+    private CityDao cityDao;
+    private EmployerDao employerDao;
 	@Autowired
-    public JobPostingManager(JobPostingDao jobPostingDao, ModelMapper modelMapper) {
+    public JobPostingManager(JobPostingDao jobPostingDao, ModelMapper modelMapper,CityDao cityDao,EmployerDao employerDao) {
         this.jobPostingDao = jobPostingDao;
         this.modelMapper=modelMapper;
+        this.cityDao=cityDao;
+        this.employerDao=employerDao;
+
     }
 
     @Override
     public Result add(JobPostingForEmployerDto jobPostingForEmployerDto) {
-    JobPosting jobPosting=modelMapper.map(jobPostingForEmployerDto,JobPosting.class);
+
+	/*    modelMapper.addMappings(new PropertyMap<jobPostingForEmployerDto,JobPosting>() {
+	        @Override
+            protected void configure(){
+	            map()
+            }
+        })*/
+
+        /*JobPosting jobPosting=modelMapper.map(jobPostingForEmployerDto,JobPosting.class);
+
         jobPosting.setApprovalStatus(false);
         jobPosting.setActivate(false);
-    jobPostingDao.save(jobPosting);
-    return new SuccessResult(Messages.Success);
+         jobPostingDao.save(jobPosting);*/
+        JobPosting jobPosting=new JobPosting();
+        jobPosting.setApplicationDeadline(jobPostingForEmployerDto.getApplicationDeadline());
+        jobPosting.setCity(this.cityDao.getById(jobPostingForEmployerDto.getCityId()));
+        jobPosting.setEmployer(this.employerDao.findById(jobPostingForEmployerDto.getEmployerId()));
+        jobPosting.setJobDescription(jobPostingForEmployerDto.getJobDescription());
+        jobPosting.setNumberOfApplication(jobPostingForEmployerDto.getNumberOfApplication());
+
+        jobPostingDao.save(jobPosting);
+         return new SuccessResult(Messages.Success);
     }
 
     @Override
--- HrmsCF/src/main/java/com/sadik/hrmscf/entities/dtos/JobSeekerRegisterDto.java
+++ HrmsCF/src/main/java/com/sadik/hrmscf/entities/dtos/JobSeekerRegisterDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class JobSeekerRegisterDto
 {
+    private int userId;
     private String firstName;
     private String lastName;
     private String email;
